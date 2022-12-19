a34c1e2103aee8ac85b7565836330ea3ecf47c92
Nikita Fedosenko <nikita.fedosenko123@gmail.com>
2022-12-19 13:32:15 +0200

    test cleanup, removed APIAdapter

--- src/__tests__/test.test.ts
+++ src/__tests__/test.test.ts
@@ -1,14 +1,12 @@
-import APIAdapter from '../api/APIAdapter'
 import SimAPI from '../api/sim/SimAPI'
 
 describe('Test of tests', () => {
     const simAPI = new SimAPI({dbUrl: '', accessToken: ''})
-    const api = new APIAdapter(simAPI)
 
     // console.log('test', api.citeQuest('test', 'test'))
 
     it('Returns Boo 25', () => {
-        console.log(api)
+        console.log(simAPI)
         const str = 'Boo 25'
         expect(str).toBe('Boo 25')
     })
