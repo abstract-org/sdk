110572fe77111e4e41b42ad66f56ee25ff98bedc
Nikita Fedosenko <nikita.fedosenko123@gmail.com>
2023-01-30 14:08:31 +0200

    change Persistence Pool and Wallet layers to return IData AD-146

--- src/interfaces/IPoolPersistance.ts
+++ src/interfaces/IPoolPersistance.ts
@@ -1,15 +1,18 @@
-import { IPool } from './IPool'
+import { IPool, IPoolCreate, IPoolQueryUpdate } from './IPool'
+import { PoolType } from '../types'
 
 export interface IPoolPersistence {
     getPools(poolHashes: Array<string>): Promise<Array<IPool>>
 
     getPoolsByKind(kind: string, limit: number): Promise<Array<IPool>>
 
+    getPoolsByType(type: PoolType, limit: number): Promise<Array<IPool>>
+
     getPoolsByCitationsCount(citationCount: number, limit: number): Promise<Array<IPool>>
 
     getPoolsByPriceRange(priceMin: number, priceMax: number, limit: number): Promise<IPool>
 
-    savePool(data: IPool): Promise<IPool>
+    savePool(data: IPoolCreate): Promise<IPool>
 
-    updatePool(questId: number, data: IPool): Promise<IPool>
+    updatePool(questId: number, data: IPoolQueryUpdate): Promise<IPool>
 }
--- src/interfaces/IWalletPersistance.ts
+++ src/interfaces/IWalletPersistance.ts
@@ -1,11 +1,11 @@
-import { IWallet } from './IWallet'
+import { IWallet, IWalletCreate, IWalletQueryUpdate } from './IWallet'
 
 export interface IWalletPersistance {
     getWallets(walletHashes: Array<string>): Promise<Array<IWallet>>
 
     getWalletsWithQuests(questHashes: Array<string>, limit: number): Promise<Array<IWallet>>
 
-    saveWallet(data: IWallet): Promise<IWallet>
+    saveWallet(data: IWalletCreate): Promise<IWallet>
 
-    updateWallet(questId: number, data: IWallet): Promise<IWallet>
+    updateWallet(questId: number, data: IWalletQueryUpdate): Promise<IWallet>
 }
