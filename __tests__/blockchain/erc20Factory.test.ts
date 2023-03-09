668e2a447f9e4a8ec40cda07c8ce4ab6bcacb1b9
Nikita Fedosenko <nikita.fedosenko123@gmail.com>
2023-03-09 18:29:56 +0200

    fixed events fetching after transaction, moved web3 init to util file

--- __tests__/blockchain/erc20Factory.test.ts
+++ __tests__/blockchain/erc20Factory.test.ts
@@ -1,22 +1,22 @@
-import Web3 from 'web3'
 import FactoryABI from '../../src/blockchain/abi/FactoryABI.json'
 import { AbiItem, AbiType } from 'web3-utils'
-
+import {web3, provider} from '@/blockchain/utils/web3'
 // Currently hardcoded, replace with your own addresses, will be configured via client/env later
 const NODE_URL = 'http://127.0.0.1:8545'
-const DEPLOYER_ADDRESS = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
-const FACTORY_ADDRESS = '0x637E18ADc9aE4029274513A081CE937cE9EF7780'
+const DEPLOYER_ADDRESS = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'
+const FACTORY_ADDRESS = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
 
 const retypedABI: AbiItem[] = FactoryABI.abi.map((abiItem: any) => ({
     ...abiItem,
     type: abiItem.type as AbiType
 }))
 
-const web3 = new Web3(NODE_URL)
-
 const erc20Factory = new web3.eth.Contract(retypedABI, FACTORY_ADDRESS)
 
 describe('Test erc20Factory interaction through Web3.js', () => {
+    afterAll(() => {
+        provider.disconnect(0, 'Tests Ended');
+    })
     it('Creates new ERC20 token', async () => {
         const tokenName = 'MyTokenTest'
         const tokenSymbol = 'MTT'
@@ -33,21 +33,7 @@ describe('Test erc20Factory interaction through Web3.js', () => {
             .createToken(tokenName, tokenSymbol, 20000, DEPLOYER_ADDRESS)
             .send({ from: DEPLOYER_ADDRESS })
 
-        //await tx.wait() <- should exist, maybe not on this scope
-
-        const events = await erc20Factory.getPastEvents('tokenCreated', {
-            fromBlock: 0,
-            toBlock: 'latest'
-        })
-
-        const events2 = await erc20Factory.getPastEvents('preDeploy', {
-            fromBlock: 0,
-            toBlock: 'latest'
-        })
-
-        // events should not be zero, they need to be emitted from contracts
-        console.log(events)
-        console.log(events2)
+        console.log('Transaction events: ', tx.events)
 
         /* expect(receipt.events.TokenCreated.returnValues.tokenName).toEqual(
             tokenName
