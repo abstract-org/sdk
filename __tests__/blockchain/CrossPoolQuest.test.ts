19691bf9ae92c4d62a115100934efa0f27773587
Nikita Fedosenko <nikita.fedosenko123@gmail.com>
2023-04-10 19:13:11 +0300

    extended crosspool tests with swap specs and tokens and pool init from predefined addresses

--- __tests__/blockchain/CrossPoolQuest.test.ts
+++ __tests__/blockchain/CrossPoolQuest.test.ts
@@ -9,12 +9,22 @@ import {
 import { Web3ApiConfig } from '@/api/web3/Web3API'
 import { Pool } from '@/blockchain/modules'
 import { FeeAmount } from '@uniswap/v3-sdk'
+import SimpleTokenABI from '@/blockchain/abi/SimpleToken.json'
+
+const wethAddress = String(process.env.WETH_ADDRESS)
+const tokenAAddress = String(process.env.TOKEN_A_ADDRESS)
+const tokenBAddress = String(process.env.TOKEN_B_ADDRESS)
+
+const wethTokenAPoolAddress = String(process.env.POOL_WETH_A_ADDRESS)
+const wethTokenBPoolAddress = String(process.env.POOL_WETH_B_ADDRESS)
+
+const crossPoolABAddress = String(process.env.POOL_A_B_ADDRESS)
 
 const providerUrl = String(process.env.PROVIDER_URL)
 const privateKey = String(process.env.TEST_PRIVATE_KEY)
 const initialSupply = 100000
 
-describe('CrossPool with Quest', () => {
+describe.skip('CrossPool with Quest', () => {
     const poolParams = {
         fee: FeeAmount.LOW,
         sqrtPrice: encodePriceSqrt(100, 1),
@@ -160,5 +170,129 @@ describe('CrossPool with Quest', () => {
         console.log(
             `token0-token1 CrossPool deployed to address: ${token0token1CrossPool.poolContract.address}`
         )
+    })
+})
+
+describe('CrossPool Swap', () => {
+    let apiConfig, provider, signer
+    let tokenA, tokenB
+    let wethTokenAPool, wethTokenBPool, crossPoolAB
+
+    async function printBalances() {
+        const [tokenABalance, tokenBBalance] = await Promise.all([
+            tokenA.connect(signer).balanceOf(signer.address),
+            tokenB.connect(signer).balanceOf(signer.address)
+        ])
+
+        console.table([
+            ['TokenA Balance', ethers.utils.formatEther(tokenABalance)],
+            ['TokenB Balance', ethers.utils.formatEther(tokenBBalance)]
+        ])
+    }
+
+    async function printLiquidity() {
+        const [
+            wethTokenAPoolLiquidity,
+            wethTokenBPoolLiquidity,
+            ABPoolLiquidity
+        ] = await Promise.all([
+            await wethTokenAPool.poolContract.liquidity(),
+            await wethTokenBPool.poolContract.liquidity(),
+            await crossPoolAB.poolContract.liquidity()
+        ])
+
+        console.table([
+            [
+                'WETH-TokenA Liquidity',
+                ethers.utils.formatEther(wethTokenAPoolLiquidity)
+            ],
+            [
+                'WETH-TokenB Liquidity',
+                ethers.utils.formatEther(wethTokenBPoolLiquidity)
+            ],
+            [
+                'TokenA-TokenB Liquidity',
+                ethers.utils.formatEther(ABPoolLiquidity)
+            ]
+        ])
+    }
+
+    beforeAll(async () => {
+        provider = new ethers.providers.JsonRpcProvider(providerUrl)
+        const wallet = new ethers.Wallet(privateKey, provider)
+        signer = wallet.connect(provider)
+
+        apiConfig = {
+            provider,
+            signer,
+            contracts: {
+                ...initializeUniswapContracts(signer),
+                tokenFactory: initializeTokenFactory(signer)
+            },
+            defaultToken: initializeDefaultToken(signer)
+        }
+
+        tokenA = new ethers.Contract(tokenAAddress, SimpleTokenABI.abi, signer)
+
+        tokenB = new ethers.Contract(tokenBAddress, SimpleTokenABI.abi, signer)
+
+        wethTokenAPool = await Pool.create(
+            wethAddress,
+            tokenAAddress,
+            500,
+            apiConfig
+        )
+        wethTokenAPool.initPoolContract(wethTokenAPoolAddress)
+
+        wethTokenBPool = await Pool.create(
+            wethAddress,
+            tokenBAddress,
+            500,
+            apiConfig
+        )
+        wethTokenBPool.initPoolContract(wethTokenBPoolAddress)
+
+        crossPoolAB = await Pool.create(
+            tokenAAddress,
+            tokenBAddress,
+            500,
+            apiConfig
+        )
+        crossPoolAB.initPoolContract(crossPoolABAddress)
+    })
+
+    beforeEach(async () => {
+        await printBalances()
+        await printLiquidity()
+    })
+
+    test('Adds liquidity to WETH-A', async () => {
+        const { tick } = await wethTokenAPool.getPoolStateData()
+
+        await wethTokenAPool.openPosition('1', tick)
+    })
+
+    test('Adds liquidity to WETH-B', async () => {
+        const { tick } = await wethTokenBPool.getPoolStateData()
+
+        await wethTokenBPool.openPosition('1', tick)
+    })
+
+    test('Buy tokenA with WETH in WETH-A Pool', async () => {
+        const amount = '0.1'
+
+        await wethTokenAPool.swapExactInputSingle(amount)
+    })
+
+    test('Buy tokenB with WETH in WETH-B Pool', async () => {
+        const amount = '0.1'
+
+        await wethTokenBPool.swapExactInputSingle(amount)
+    })
+
+    test('Buy tokenB with tokenA in A-B Pool', async () => {
+        const amount = '1'
+
+        await crossPoolAB.swapExactInputSingle(amount)
     })
 })
